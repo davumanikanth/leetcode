8bf0fe3f6d1267beb44409f06cd2ed4feee53969
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-05-25 15:45:16 +0530

    Time: 3 ms (55.68%) | Memory: 43.7 MB (14.14%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,37 +1,60 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int m=nums2.length;
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        Set<Integer> msri=new LinkedHashSet<>();
-        int index=0;
-        int i=0;
-        int j=0;
-        while(i<n && j<m)
-        {
-            if(nums1[i]<nums2[j])
-            {
-                i++;
-            }
-            else if(nums1[i]>nums2[j])
-            {
-                j++;
-            }
-            else
-            {
-                msri.add(nums1[i]);
-                 i++;
-                 j++;
+        // using two pointer
+        // int n=nums1.length;
+        // int m=nums2.length;
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // Set<Integer> msri=new LinkedHashSet<>();
+        // int index=0;
+        // int i=0;
+        // int j=0;
+        // while(i<n && j<m)
+        // {
+        //     if(nums1[i]<nums2[j])
+        //     {
+        //         i++;
+        //     }
+        //     else if(nums1[i]>nums2[j])
+        //     {
+        //         j++;
+        //     }
+        //     else
+        //     {
+        //         msri.add(nums1[i]);
+        //          i++;
+        //          j++;
 
+        //     }
+        // }
+        // int[] sri=new int[msri.size()];
+        // int love=0;
+        // for(int num : msri)
+        // {
+        //     sri[love++]=num;
+        // }
+        // return sri;
+        
+        //usnig linkedhashset
+        Set<Integer> set1=new LinkedHashSet<>();
+        for(int n: nums1)
+        {
+            set1.add(n);
+        }
+        Set<Integer> set2=new LinkedHashSet<>();
+        for(int n:nums2)
+        {
+            if(set1.contains(n))
+            {
+                set2.add(n);
             }
         }
-        int[] sri=new int[msri.size()];
-        int love=0;
-        for(int num : msri)
-        {
-            sri[love++]=num;
-        }
-        return sri;
+         int[] newarray=new int[set2.size()];
+         int index=0;
+         for(int n: set2)
+         {
+            newarray[index++]=n;
+         }
+       return newarray;
     }
 }
