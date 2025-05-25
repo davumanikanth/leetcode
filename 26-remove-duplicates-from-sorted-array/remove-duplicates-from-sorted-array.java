6ba5a1bf3c94e39cfbab946082d0a8e5578c784c
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-05-25 15:34:25 +0530

    Time: 1 ms (61.61%) | Memory: 45 MB (40.22%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,43 +1,43 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        Set<Integer> uniqelement=new LinkedHashSet<>();
-        for(int num: nums)
-        {
-            uniqelement.add(num);
-        }
-        int index=0;
-        for(int num: uniqelement)
-        {
-            nums[index++]=num;
-        }
-        
-        
-        return index;
-
-
-        // int[] love=new int[n];
+        // int n=nums.length;
+        // Arrays.sort(nums);
+        // Set<Integer> uniqelement=new LinkedHashSet<>();
+        // for(int num: nums)
+        // {
+        //     uniqelement.add(num);
+        // }
         // int index=0;
-        // for(int i=0;i<n-1;i++)
+        // for(int num: uniqelement)
         // {
-          
-        //         if(nums[i]==nums[i+1])
-        //         {
-        //             continue;
-        //         }
-        //         else
-        //         {
-        //             love[index]=nums[i];
-        //             index++;
-        //         }
-            
+        //     nums[index++]=num;
         // }
-        // love[index++]=nums[n-1];
-        // for(int i=0;i<index;i++)
-        // {
-        //     nums[i]=love[i];
-        // }
+        
+        
         // return index;
+
+int n=nums.length;
+        int[] love=new int[n];
+        int index=0;
+        for(int i=0;i<n-1;i++)
+        {
+          
+                if(nums[i]==nums[i+1])
+                {
+                    continue;
+                }
+                else
+                {
+                    love[index]=nums[i];
+                    index++;
+                }
+            
+        }
+        love[index++]=nums[n-1];
+        for(int i=0;i<index;i++)
+        {
+            nums[i]=love[i];
+        }
+        return index;
     }
 }
