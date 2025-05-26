ae5cef18d154ad31063a7f23bf59c6487e2ce4a8
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-05-27 01:21:02 +0530

    Time: 4 ms (45.55%) | Memory: 91.6 MB (5.63%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,26 +1,50 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n=nums.length;
-        ArrayList<Integer> positive=new ArrayList<>();
-        ArrayList<Integer> negative=new ArrayList<>();
-
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>0)
-            {
-                positive.add(nums[i]);
+        int n = nums.length;
+        int[] pos = new int[n / 2];
+        int[] neg = new int[n / 2];
+        int k1 = 0;
+        int k2 = 0;
+        for(int i = 0;i < n;i++) {
+            if(nums[i] > 0){
+                pos[k1] = nums[i];
+                k1++;
             }
-            else
-            {
-                negative.add(nums[i]);
+            else{
+                neg[k2] = nums[i];
+                k2++;
             }
         }
-        for(int i=0;i<n/2;i++)
-        {
-            nums[2*i]=positive.get(i);
-            nums[2*i+1]=negative.get(i);
+        for(int i = 0;i < n / 2;i++) {
+            nums[2 * i] = pos[i];
+            nums[2 * i + 1] = neg[i];
         }
-       return nums;
-        
+        return nums;
     }
 }
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         int n=nums.length;
+//         ArrayList<Integer> positive=new ArrayList<>();
+//         ArrayList<Integer> negative=new ArrayList<>();
+
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]>0)
+//             {
+//                 positive.add(nums[i]);
+//             }
+//             else
+//             {
+//                 negative.add(nums[i]);
+//             }
+//         }
+//         for(int i=0;i<n/2;i++)
+//         {
+//             nums[2*i]=positive.get(i);
+//             nums[2*i+1]=negative.get(i);
+//         }
+//        return nums;
+
+//     }
+// }
