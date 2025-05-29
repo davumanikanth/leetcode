2b22d3a46ca44c104ced4ae647835a81307ca242
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-05-29 12:57:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (9.22%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
          int n=mat.length;
+         //step1:- write the code for rotate of matrix in 90 degrees
+         //step2:- write the code to check the rotated matrix is same as the target 
+         //step3:- write the code the rotate the matrix in all the side and check with target
+         //step3
          for(int i=0;i<4;i++)
          {
             
@@ -14,7 +18,7 @@ class Solution {
 
        
     } 
-    
+    //step1
     public int[][] rotate(int[][] nums)
     {
         int n=nums.length;
@@ -29,6 +33,7 @@ class Solution {
         return rotatedmatrix;
 
     }
+    //step2
     public boolean issame(int[][] a,int[][] b)
     {
         int n=a.length;
