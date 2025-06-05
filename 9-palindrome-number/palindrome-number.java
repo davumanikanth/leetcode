6a04037c0a9910286810bf16e05f7c6019cf441e
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-06-05 22:55:36 +0530

    Time: 5 ms (69.81%) | Memory: 44.2 MB (38.15%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-     int temp=x;
-     int reverse=0;
+     
+     //based on test cases if the number is negative number return false;
       if(x<0)
      {
         return false;
      }
+     //we need to store the value of x to temp 
+     int temp=x;
+     int reverse=0;
+
+     //if the number is positive we need to find the reverse of the number and we need to match to the temp if it matches we need to return true; 
      while(x!=0)
      {
       int digit=x%10;
