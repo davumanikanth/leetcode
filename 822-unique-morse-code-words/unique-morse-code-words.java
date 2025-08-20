29b630aafe9dde7ee3a87fa118fd8365e3cab437
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-08-20 19:08:34 +0530

    Time: 3 ms (37.44%) | Memory: 41.8 MB (42.06%) - LeetSync

---
+++ 822-unique-morse-code-words/unique-morse-code-words.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        HashMap<Character,String> map=new HashMap<>();
+        map.put('a',".-");
+        map.put('b',"-...");
+        map.put('c',"-.-.");
+        map.put('d',"-..");
+        map.put('e',".");
+        map.put('f',"..-.");
+        map.put('g',"--.");
+        map.put('h',"....");
+        map.put('i',"..");
+        map.put('j',".---");
+        map.put('k',"-.-");
+        map.put('l',".-..");
+        map.put('m',"--");
+        map.put('n',"-.");
+        map.put('o',"---");
+        map.put('p',".--.");
+        map.put('q',"--.-");
+        map.put('r',".-.");
+        map.put('s',"...");
+        map.put('t',"-");
+        map.put('u',"..-");
+        map.put('v',"...-");
+        map.put('w',".--");
+        map.put('x',"-..-");
+        map.put('y',"-.--");
+        map.put('z',"--..");
+        HashSet<String > result=new HashSet<>();
+        for(int i=0;i<words.length;i++)
+        {
+            StringBuilder sb=new StringBuilder();
+            for(int j=0;j<words[i].length();j++)
+            
+            {
+                char ch=words[i].charAt(j);
+                sb.append(map.get(ch));
+            }
+            result.add(sb.toString());
+        }
+        return result.size();
+    }
+}
