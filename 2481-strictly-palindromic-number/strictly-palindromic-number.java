5ecb139bf187a19843def07085a2810b3842bae9
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-08-30 15:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (70.31%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,24 +1,5 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        String[] res=new String[n-3];
-        int index=0;
-        for(int i=2;i<=n-2;i++)
-        {
-           
-          res[index++]=Integer.toString(n,i);
-
-
-        }
-        for(int i=0;i<index;i++)
-        {
-            String rev=new StringBuilder(res[i]).reverse().toString();
-            if(!res[i].equals(res))
-            {
-                return false;
-
-            }
-        }
-        return true;
-        
+        return false;
     }
 }
