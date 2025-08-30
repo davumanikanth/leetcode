e152c89bf7f843ae3983e610327d553224c54a32
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-08-30 14:01:22 +0530

    Time: 4 ms (93.69%) | Memory: 45.5 MB (47.08%) - LeetSync

---
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n;i++)
+        {
+            boolean[] seen=new boolean[n+1];
+            for(int j=0;j<n;j++)
+            {
+                int num=matrix[i][j];
+                if(num<1 || num>n || seen[num])
+                {
+                    return false;
+                }
+                seen[num]=true;
+
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            boolean[] seen=new boolean[n+1];
+            for(int j=0;j<n;j++)
+            {
+                int num=matrix[j][i];
+                if(num<1 || num>n || seen[num])
+                {
+                    return false;
+                }
+                seen[num]=true;
+
+            }
+        }
+        return true;
+    }
+}
