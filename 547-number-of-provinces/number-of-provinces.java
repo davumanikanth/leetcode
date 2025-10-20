e2afcadffd2f3a786b080cb346d8ef685623d0d0
manikanth <148421168+davumanikanth@users.noreply.github.com>
2025-10-20 16:22:46 +0530

    Time: 3 ms (33.35%) | Memory: 46.8 MB (90.51%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,78 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+ 
+    ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+
+    /// creating an empty list
+    for(int i=0;i<isConnected.length;i++)
+    {
+        adj.add(new ArrayList<>());
+    }
+
+  //collectin the adj list
+
+    for(int i=0;i<isConnected.length;i++)
+    {
+        for(int j=0;j<isConnected.length;j++)
+        {
+            if(isConnected[i][j]==1 && i!=j)
+            {
+                adj.get(i).add(j);
+                adj.get(j).add(i);
+                
+            }
+        }
+    }
+
+   boolean[] visited=new boolean[isConnected.length];
+   int count=0;
+
+   int n=isConnected.length;
+   for(int i=0;i<n;i++)
+   {
+    if(visited[i]==false)
+    {
+        count++;
+        dfs(i,adj,visited);
+
+    }
+   }
+   
+
+       
+    return count;
+
+
+        
+    }
+    public void dfs(int node,ArrayList<ArrayList<Integer>> adj,boolean[] visited )
+    {
+        visited[node]=true;
+        for(int con:adj.get(node))
+        {
+            if(visited[con]==false)
+            {
+            dfs(con,adj,visited);
+            }
+        }
+    }
+    public void bfs(int node,ArrayList<ArrayList<Integer>> adj,boolean[] visited)
+    {
+        Queue<Integer> q=new LinkedList<>();
+         q.offer(node);
+         visited[node]=true;
+
+         while(!q.isEmpty())
+         {
+            int rem=q.poll();
+            for(int con:adj.get(node))
+            {
+                if(visited[con]==false)
+                {
+                    q.offer(con);
+                }
+            }
+         }
+
+    }
+}
