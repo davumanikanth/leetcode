e1c0de9c96cc26ba7b1eec6635883ba0a001a92d
manikanth <148421168+davumanikanth@users.noreply.github.com>
2026-01-29 20:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (69.64%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return null;
+        if(root.val==key)
+        {
+            return helper(root);
+        }
+        TreeNode dummy=root;
+        while(root!=null)
+        {
+
+        
+        if(root.val>key)
+        {
+            if(root.left!=null && root.left.val==key)
+            {
+                root.left=helper(root.left);
+                
+
+            }
+            else
+            {
+                root=root.left;
+            }
+
+        }
+        else
+          {
+            if(root.right!=null && root.right.val==key)
+            {
+                root.right=helper(root.right);
+                
+            
+
+            }
+            else
+            {
+                root=root.right;
+            }
+
+        }
+        }
+        return dummy;
+        
+    }
+    public TreeNode helper(TreeNode node)
+    {
+        if(node.left==null) return node.right;
+        else if(node.right==null) return node.left;
+        TreeNode rightchild=node.right;
+        TreeNode leftchild= leftrightmost(node.left);
+        leftchild.right=rightchild;
+        return node.left;
+
+    }
+    public TreeNode leftrightmost(TreeNode node)
+    {
+        if(node==null) return null;
+        while(node.right!=null)
+        {
+            node=node.right;
+
+        }
+        return node;
+    }
+}
