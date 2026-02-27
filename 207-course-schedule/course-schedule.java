ea898f99fb038b32bc394b02cb0ce7ad29aa3e49
manikanth <148421168+davumanikanth@users.noreply.github.com>
2026-02-28 00:13:06 +0530

    Time: 7 ms (61.14%) | Memory: 47.3 MB (34.41%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,60 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n=prerequisites.length;
+        int[] indegree=new int[numCourses];
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++)
+        {
+            int v=prerequisites[i][0];
+            int u=prerequisites[i][1];
+            adj.get(u).add(v);
+        }
+        
+
+
+        for(int i=0;i<numCourses;i++)
+        {
+            for(int j=0;j<adj.get(i).size();j++)
+            {
+                int nei=adj.get(i).get(j);
+                indegree[nei]++;
+            }
+        }
+
+
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<indegree.length;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.add(i);
+            }
+        }
+
+       ArrayList<Integer> result=new ArrayList<>();
+        while(!q.isEmpty())
+        {
+            int node=q.remove();
+            result.add(node);
+            for(int i=0;i<adj.get(node).size();i++)
+            {
+                int ni=adj.get(node).get(i);
+                indegree[ni]--;
+                if(indegree[ni]==0)
+                {
+                    q.add(ni);
+                }
+            }
+        
+        }
+        if(result.size()!=numCourses)
+        {
+            return false;
+        }
+        return true;
+    }
+}
