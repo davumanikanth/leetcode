e9d4c0dce5e3a7e6d0291055cb92ed273cfb7bba
manikanth <148421168+davumanikanth@users.noreply.github.com>
2026-04-08 23:20:10 +0530

    Time: 61 ms (46.67%) | Memory: 47.7 MB (78.67%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
@@ -1,31 +1,36 @@
 class Solution {
-
-
     public int xorAfterQueries(int[] nums, int[][] queries) {
-     long mod = 1000000007;
-        // Process each query
-        for (int[] t : queries) {
-            int l = t[0];
-            int r = t[1];
-            int k = t[2];
-            int v = t[3];
+       long mod=1000000007;
+       for(int i=0;i<queries.length;i++)
+       {
+         int l=queries[i][0];
+         int r=queries[i][1];
+         int k=queries[i][2];
+         int v=queries[i][3];
+         int index=l;
 
-            int idx = l;
+         while(index<=r)
+         {
+            long temp=nums[index];
+            nums[index]=(int)((temp*v) % mod);
+            index+=k;
 
-            // Apply operation at step k
-            while (idx <= r) {
-                long temp = nums[idx];
-                nums[idx] = (int)((temp * v) % mod);
-                idx += k;
-            }
-        }
+         }
+       }
+         
 
-        // Compute XOR of final array
-        int ans = 0;
-        for (int num : nums) {
-            ans ^= num;
-        }
+         //xor for the nums
+         int xor=0;
 
-        return ans;
+         for(int x=0;x<nums.length;x++)
+         {
+            xor=xor^ nums[x];
+
+         }
+         return xor;
+         
+         
+         
+                
     }
 }
