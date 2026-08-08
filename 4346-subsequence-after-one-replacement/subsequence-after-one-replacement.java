d829742e88214d48bc5e444fb0c558d6e98b7f6a
manikanth <148421168+davumanikanth@users.noreply.github.com>
2026-08-08 14:54:26 +0530

    Time: 18 ms (18.68%) | Memory: 47.8 MB (87.98%) - LeetSync

---
+++ 4346-subsequence-after-one-replacement/subsequence-after-one-replacement.java
@@ -0,0 +1,81 @@
+class Solution {
+
+    public boolean solve(int i, int j, String s, String t) {
+
+        if (i < 0) return true;
+
+        if (j < 0) return false;
+
+        if (s.charAt(i) == t.charAt(j)) {
+            return solve(i - 1, j - 1, s, t);
+        }
+
+        return solve(i, j - 1, s, t);
+    }
+
+    public boolean canMakeSubsequence(String s, String t) {
+
+        if (s.length() > t.length()) return false;
+
+        int[] prefix =new int[s.length()];
+        Arrays.fill(prefix, -1);
+        int[] suffix=new int[s.length()];
+        Arrays.fill(suffix, -1);
+        int i=0;
+        int j=0;
+        
+        while(i < s.length() && j < t.length())
+        {
+            if(s.charAt(i)==t.charAt(j))
+            {
+                prefix[i]=j;
+                i++;
+                j++;
+                
+            }
+            else{
+
+            j++;
+            }
+        }
+        
+        int x=s.length()-1;
+        int y=t.length()-1;
+        while(x >= 0 && y >= 0)
+        {
+            if(s.charAt(x)==t.charAt(y))
+            {
+                suffix[x]=y;
+                x--;
+                y--;
+            }
+            else
+            {
+                y--;
+            }
+        }
+
+        for(int k=0;k<prefix.length;k++)
+        {
+            if (k > 0 && prefix[k - 1] == -1) {
+        continue;
+    }
+
+    // Suffix after k must be matchable
+    if (k < prefix.length - 1 && suffix[k + 1] == -1) {
+        continue;
+    }
+            
+            int left = (k == 0) ? -1 : prefix[k- 1];           
+            int right=(k==prefix.length-1)? t.length(): suffix[k+1];
+            if(left+1<right) 
+            {
+                return true;
+              
+            
+            } 
+        }
+      return false;
+
+    }
+}
